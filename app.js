36b4dfe45db0f68ea7482eb89a723b3a9b31ff88
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-27 14:24:46 -0500

    added toggle shift event

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-    $("#keyboard-upper-container").hide()
-})
+$(document).ready(function () {
+    $('#keyboard-lower-container').show();
+    $('#keyboard-upper-container').hide();
+    
+    $(document).keydown(function (e) {
+        if (e.keyCode == 16) {
+            $('#keyboard-lower-container').toggle();
+            $('#keyboard-upper-container').toggle();
+        }
+    });
+});
+
