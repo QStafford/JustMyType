c0939b00d584f901ef62747b216bd1a7bb808bb6
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-27 16:59:55 -0500

    Adjusted

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ $(document).ready(function () {
             $('#keyboard-upper-container').hide();
         }
     });
+   $(document).keypress(function (e){
+        if (e.keyCode == 113) {
+            $("#113").css("background-color", "yellow")
+        }
+    })
+     
     let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
     $("#sentence").text(sentences[0])
 });
